21a716afcc9cbc83e58cfcd7a43fd38aeddf33e2
udaikmr96-pixel <udaikmr96@gmail.com>
2026-07-09 09:18:25 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-    from fastapi import FastAPI, APIRouter, HTTPException, Depends, Header
+from fastapi import FastAPI, APIRouter, HTTPException, Depends, Header
     from dotenv import load_dotenv
     from starlette.middleware.cors import CORSMiddleware
     from motor.motor_asyncio import AsyncIOMotorClient
